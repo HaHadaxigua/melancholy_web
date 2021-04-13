7d6ae1389c180c2e133037dbd6bf037e83beef1c
blade <dh@bladetekiAir.mshome.net>
2021-04-13 10:11:39 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const routes = [
     {
         path: '/',
         name: 'index',
-        component: () => import('../views/home.vue')
+        component: () => import('../views/Welcome.vue')
     },
     {
         path: '/login',
