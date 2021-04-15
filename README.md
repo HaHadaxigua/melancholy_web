e6e4d277871ed7c5f9b3cf97fd058303bc7886d6
HaHadaxigua <hahadaxigua@gmail.com>
2021-04-15 11:58:00 +0800

    update

--- README.md
+++ README.md
@@ -6,3 +6,27 @@ Vite 是一个由原生 ES 模块驱动的 Web 开发构建工具，支持模块
 1. `npm install vite-app melancholy`
 2. `yarn` 打包项目
 3. `yarn dev` 运行服务
+
+# 2. 项目结构
+├─ public             # 静态资源
+├─ src                # 源码
+│  ├─ @types          # ts 声明
+│  ├─ apis            # 接口请求
+│  ├─ assets          # webpack打包的资源
+│  ├─ components      # 公共组件
+│  ├─ config          # 全部配置
+│  ├─ constant        # 常量
+│  ├─ directives      # 全局指令
+│  ├─ layout          # 全局Layout
+│  ├─ locales         # 国际化
+│  ├─ model           # 全部model存放
+│  ├─ plugins         # 插件
+│  ├─ router          # 路由
+│  ├─ store           # 全局store管理
+│  ├─ styles          # 全局样式
+│  ├─ utils           # 全局公共方法
+│  └─ views           # 所有业务页面
+├─ tsconfig.json      # ts 编译配置
+└─ vue.config.js      # vue-cli 配置
+————————————————
+
---
+++ src/layout/components/navigation_bar/Index.vue
@@ -0,0 +1,36 @@
+<!--导航栏header-->
+<template>
+  <el-breadcrumb
+      class="app-breadcrumb"
+      separator-class="el-icon-arrow-right"
+  >
+    <transition-group name="breadcrumb">
+      <el-breadcrumb-item
+          v-for="(item, index) in breadcrumbs"
+          :key="item.path"
+      >
+        <span
+            v-if="item.redirect === 'noredirect' || index === breadcrumbs.length-1"
+            class="no-redirect"
+        >{{ t('route.' + item.meta.title) }}</span>
+        <a
+            v-else
+            @click.prevent="handleLink(item)"
+        >{{ t('route.' + item.meta.title) }}</a>
+      </el-breadcrumb-item>
+    </transition-group>
+  </el-breadcrumb>
+</template>
+
+<script lang="ts">
+import {compile} from 'path-to-regexp'
+import router from './router.vue'
+
+export default {
+  name: "Index"
+}
+</script>
+
+<style scoped>
+
+</style>
